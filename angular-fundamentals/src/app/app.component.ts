8169dd0489ed51ed61cf5cfca884f914a9e025e1
timscriv <tscrivner1@gmail.com>
2016-12-20 13:14:18 -0600

    added simple service

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World!';
+  
+  constructor(private mail: MailService) { }
+
 }
